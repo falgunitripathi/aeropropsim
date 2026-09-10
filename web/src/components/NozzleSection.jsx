6743c477111cede7f9c734876cfee733ad5de064
Falguni <falgunitripathi8@gmail.com>
2026-09-10 11:58:54 +0530

    Update NozzleSection.jsx

--- web/src/components/NozzleSection.jsx
+++ web/src/components/NozzleSection.jsx
@@ -21,7 +21,7 @@ export default function NozzleSection({ config, onChange }) {
   return (
     <fieldset className="config-section">
       <legend>
-        <button type="button" className="disclosure" onClick={() => setOpen(!open)}>
+        <button type="button" className="disclosure" aria-expanded={open} onClick={() => setOpen(!open)}>
           {open ? "▾" : "▸"} Nozzle
         </button>
       </legend>
